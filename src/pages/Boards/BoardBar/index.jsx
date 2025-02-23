e30e9aadcdc7f8e14a0678ac6e8cb149dd546860
Kieu Huynh <anhkieu303252@gmail.com>
2025-02-23 19:42:08 +0700

    feat: Enhance BoardBar with new icons, chips, and invite button

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -1,17 +1,109 @@
+import AddToDriveIcon from '@mui/icons-material/AddToDrive'
+import BoltIcon from '@mui/icons-material/Bolt'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import FilterListIcon from '@mui/icons-material/FilterList'
+import PersionAddIcon from '@mui/icons-material/PersonAdd'
+import VpnLockIcon from '@mui/icons-material/VpnLock'
+import Avatar from '@mui/material/Avatar'
+import AvatarGroup from '@mui/material/AvatarGroup'
 import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Chip from '@mui/material/Chip'
+import Tooltip from '@mui/material/Tooltip'
+
+const MENU_STYLE = {
+  color: 'primary.main',
+  bgcolor: 'white',
+  border: 'none',
+  paddingX: '5px',
+  borderRadius: '4px',
+  '& .MuiSvgIcon-root': {
+    color: 'primary.main'
+  },
+  '&:hover': {
+    bgcolor: 'primary.50'
+  }
+}
 
 function BoardBar() {
   return (
     <Box
       sx={{
-        backgroundColor: 'primary.dark',
+        // backgroundColor: 'primary.dark',
         width: '100%',
         height: theme => theme.trello.boardBarHeight,
         display: 'flex',
-        alignItems: 'center'
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        gap: 2,
+        px: 2,
+        overflowX: 'auto',
+        borderTop: '2px solid #00bfa5'
       }}
     >
-      Board Bar
+      <Box display="flex" alignItems="center" gap={2}>
+        <Chip
+          sx={MENU_STYLE}
+          icon={<DashboardIcon />}
+          label="Khuynh Dev Board"
+          clickable
+          onClick={() => {}}
+        />
+        <Chip
+          sx={MENU_STYLE}
+          icon={<VpnLockIcon />}
+          label="Public/Private Workspace"
+          clickable
+          onClick={() => {}}
+        />
+        <Chip
+          sx={MENU_STYLE}
+          icon={<AddToDriveIcon />}
+          label="Add to Google Drive"
+          clickable
+          onClick={() => {}}
+        />
+        <Chip
+          sx={MENU_STYLE}
+          icon={<BoltIcon />}
+          label="Automation"
+          clickable
+          onClick={() => {}}
+        />
+        <Chip
+          sx={MENU_STYLE}
+          icon={<FilterListIcon />}
+          label="Filters"
+          clickable
+          onClick={() => {}}
+        />
+      </Box>
+      <Box display="flex" alignItems="center" gap={2}>
+        <Button variant="outlined" startIcon={<PersionAddIcon />}>
+          Invite
+        </Button>
+
+        <AvatarGroup
+          max={4}
+          sx={{ '& .MuiAvatar-root': { width: 34, height: 34, fontSize: 16 } }}
+        >
+          <Tooltip title="Remy Sharp" arrow>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </Tooltip>
+          <Tooltip title="Travis Howard" arrow>
+            <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+          </Tooltip>
+          <Tooltip title="Cindy Baker" arrow>
+            <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+          </Tooltip>
+          <Tooltip title="Agnes Walker" arrow>
+            <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
+          </Tooltip>
+          <Tooltip title="Trevor Henderson" arrow>
+            <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
     </Box>
   )
 }
