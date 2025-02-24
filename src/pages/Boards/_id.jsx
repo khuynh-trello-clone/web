5ec766d966bc272076c912eb3245094e23dc6a2e
K.Huynh <anhkieu303252@gmail.com>
2025-02-24 22:08:39 +0700

    feat: Integrate Axios for fetching board details and update API root

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -5,9 +5,20 @@ import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 
-import { mockData } from '~/apis/mock-data'
+import { useEffect, useState } from 'react'
+import { fetchBoardDetailsAPI } from '~/apis'
+import { BOARD_ID } from '~/utils/constants'
 
 function Board() {
+  const [board, setBoard] = useState(null)
+
+  useEffect(() => {
+    const boardId = BOARD_ID
+    fetchBoardDetailsAPI(boardId).then(board => {
+      setBoard(board)
+    })
+  }, [])
+
   return (
     <Container
       disableGutters
@@ -17,8 +28,8 @@ function Board() {
       }}
     >
       <AppBar />
-      <BoardBar board={mockData?.board} />
-      <BoardContent board={mockData?.board} />
+      <BoardBar board={board} />
+      <BoardContent board={board} />
     </Container>
   )
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
 // Constants
 export const BOARD_BAR_HEIGHT = '58px'
 export const APP_BAR_HEIGHT = '48px'
-export const API_ROOT = 'http://localhost:3000'
+export const API_ROOT = 'http://localhost:2025'
+
+export const BOARD_ID = '67bc7901cf2dcc8a70c91dbc'
